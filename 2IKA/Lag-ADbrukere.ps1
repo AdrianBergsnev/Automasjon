523ec8427590dc057e3d416ad81675d1188ab465
Adrian Bergsnev <adrian.bergsnev@gmail.com>
2017-10-23 16:44:08 +0200

    added comments

--- 2IKA/Lag-ADbrukere.ps1
+++ 2IKA/Lag-ADbrukere.ps1
@@ -1,5 +1,18 @@
 ﻿Import-module ActiveDirectory  
 
+### Kommentarer:
+## Dette skriptet er hardkodet for IKT-Klassen på Hønefoss VGS. Dette skriptet oppretter brukere, og setter dem i Elevgruppe
+## Det eneste man trenger å gjøre er å passe på at "CSV" pathen stemmer med din egen .csv fil.
+## Eksempel på hvordan hvordan en bruker ser ut på .csv filen:
+## Fornavn;Etternavn;Passord
+## Ghada;Alashqar;8EGB4MA
+## Skriptet tar de to første bokstavene i fornavn og to første i etternavn etterfulgt av årstallet:
+## (eksempel): GhAl2017
+## Klasselisten for IKT elever 2017-2018 ligger i Klasseliste.csv på samme repo
+## Output med brukernavn og passord havner i terminalen etter man har kjørt scriptet.
+## Brukerne må logge på en domenemaskin, for å der etter bli bedt om å endre passordet sitt.
+
+
 $Users = Import-Csv -Delimiter ";" -Path "C:\Users\Administrator\Desktop\Powershell\Klasseliste.csv"          
 foreach ($User in $Users)             
 {    
@@ -14,7 +27,7 @@ foreach ($User in $Users)
     $UPN = $SAM + '@' + $Domainname 
     $OU = "OU=Elever,OU=2IKA,DC=2,DC=ika,DC=no"    
     $Group = "CN=Elever,OU=Grupper,OU=2IKA,DC=2,DC=ika,DC=no"                            
-    $Password = $User.Passord 
+    $Password = $User.Passord
     
     New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName "$SAM" -UserPrincipalName $UPN -GivenName "$UserFirstname" `
     -Surname "$UserLastname" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) `
